293c248574155914125a93c05a8a9b46e92b9480
tikemin <st.j.www@gmail.com>
2021-06-14 13:47:54 +0900

    サンプルとして、RequestBodyを追加

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
     @RequestMapping(value = {"/hello", "/hello/{msg}"})
+    //@ResponseBody
 	public String index(@PathVariable(name = "msg", required = false) String msg, Model model)  {
         if(msg == null) {
             msg = "Hello World";
